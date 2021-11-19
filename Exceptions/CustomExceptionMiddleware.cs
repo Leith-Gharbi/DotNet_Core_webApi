5615716c7494b5262dd4eb99f7194005367da80b
gharbi7leith@gmail.com <gharbi7leith@gmail.com>
2021-11-19 16:46:06 +0100

    first part of custom exception middleware

---
+++ Exceptions/CustomExceptionMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace DotNet_Core_webApi.Exceptions
+{
+    public class CustomExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                await HandleExceptionAsync(httpContext, ex);
+            }
+
+        }
+
+        private Task HandleExceptionAsync(HttpContext httpContext, Exception ex)
+        {
+            httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            httpContext.Response.ContentType = "application/json";
+        }
+    }
+}
